49d9e8eb31ce86ba279446f3630153d2c6a7fa9c
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-04-22 00:41:19 -0500

    CODEFORCES EXERCISE T PYTHON

--- CODEFORCES/EXERCISE_T/exercise_t.py
+++ CODEFORCES/EXERCISE_T/exercise_t.py
@@ -1,1 +1,13 @@
+resultados = []
 #EJERCICIO 1760A  
+#LEEMOS EL NUMERO DE CASOS DE PRUEBA
+t = int(input())
+for _ in range(t):
+    #LEEMOS LOS NUMEROS DE LA PRUEBA
+    a, b, c = map(int, input().split())
+    lista = [a, b, c]
+    #ORDEN
+    lista.sort()
+    resultados.append(lista[1])
+for resultado in resultados:
+    print(resultado)
